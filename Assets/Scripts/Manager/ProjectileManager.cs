495a5276203dda6d5471f4474d899e1ee39bdda3
DDanPat <hyoung6855@gmail.com>
2025-02-23 13:43:11 +0900

    [ chore ] ProjectileManager 추가

--- Assets/Scripts/Manager/ProjectileManager.cs
+++ Assets/Scripts/Manager/ProjectileManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileController : MonoBehaviour
+public class ProjectileManager : MonoBehaviour
 {
     
 }
