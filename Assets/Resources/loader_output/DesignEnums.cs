e36d2a367a451976f2427b42dfd7f3c295c3dcf1
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-24 14:10:51 +0900

    ✨ [ feat ] 몬스터 생성 로직 추가
    랜덤한 몬스터가 랜덤한 위치에서 생성되도록 하는 로직 추가

---
+++ Assets/Resources/loader_output/DesignEnums.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class DesignEnums
+{
+    public enum MonsterType
+    {
+        Orc = 0,
+        Skeleton = 1,
+    }
+}
--- Assets/Scripts/Manager/MonsterManager.cs
+++ Assets/Scripts/Manager/MonsterManager.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterManager : SingleTon<MonsterManager>
+public class MonsterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<GameObject> monsterPrefabs;
+
+    [SerializeField] Rect spawnAreas;
+    private List<MonsterController> activeMonsters = new List<MonsterController>();
+
+    GameManager gameManager;
+
+    private void Init(GameManager gameManager)
     {
-        
+        this.gameManager = gameManager;
     }
 
-    // Update is called once per frame
-    void Update()
+    // select random monster to spawn
+    private void SpawnRandomMonster()
     {
-        
+        if (monsterPrefabs.Count == 0) { Debug.LogError("Cannot find Monster Prefabs!"); return; }
+
+        // get random monster from prefab list
+        GameObject randomPrefab = monsterPrefabs[Random.Range(0, monsterPrefabs.Count)];
+        // get random position from entire spawn area
+        Vector2 randomPosition = new Vector2(
+            Random.Range(spawnAreas.xMin, spawnAreas.xMax),
+            Random.Range(spawnAreas.yMin, spawnAreas.yMax)
+            );
+
+        GameObject spawnedMonster = Instantiate(randomPrefab, new Vector3(randomPosition.x, randomPosition.y), Quaternion.identity);
+        MonsterController monsterController = spawnedMonster.GetComponent<MonsterController>();
+
+        // init monster controller  // make monster object work
+    }
+
+    public void RemoveMonster()  
+    {
+        // remove monsters which are already dead
+        // remove monsters only if player cleared the stage
     }
 }
