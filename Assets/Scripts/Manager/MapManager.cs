6f2aa78a45e52284b7bf2b8aa7b1ddffc3c2b867
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-24 10:42:21 +0900

    ✨ [ feat ] 맵 불러오기 기능 추가
    - 맵 Prefab 3개 추가
    - 맵 불러오기를 위한 MapManager 수정

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class MapManager : SingleTon<MapManager>
+public class MapManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject[] mapPrefabs;
+
+    private int RandomMapCount()  // 
+    {   
+        int mapCounts = mapPrefabs.Count();
+        int randomMapCount = Random.Range(0, mapCounts);
+        return randomMapCount;
+    }
+    public void LoadRandomMap()  // load random map on start
     {
-        
+        if (mapPrefabs == null)  // process exception
+        { Debug.LogError("Map Prefabs are not founded!"); return; }
+
+        int randomMapCount = RandomMapCount();
+        Instantiate(mapPrefabs[randomMapCount]);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // spawn entities (player, monsters & boss)
 }
