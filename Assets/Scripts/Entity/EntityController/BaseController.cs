5303fbf6d92d1d0a0953c9ba99cdd5280f98f023
kgs1121 <kkggss1121@gmail.com>
2025-02-24 11:34:46 +0900

    [ feat ] Controller, StatHandler

--- Assets/Scripts/Entity/EntityController/BaseController.cs
+++ Assets/Scripts/Entity/EntityController/BaseController.cs
@@ -4,15 +4,123 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    protected StatHandler statHandler;
+    [SerializeField] private SpriteRenderer chracterRenderer;
+    [SerializeField] private Transform weaponPivot;
+
+    protected Rigidbody2D _rigidbody;
+    //protected AnimationHandler animationHandler;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    [SerializeField] public WeaponHandler weaponPrefab;
+    protected WeaponHandler weaponHandler;
+
+    protected bool isAttacking;
+    private float timeSinceLastAttack = float.MaxValue;
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    private Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
+    protected virtual void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+        //animationHandler = GetComponent<AnimationHandler>();
+        statHandler = GetComponent<StatHandler>();
+
+        if (weaponPrefab != null)
+        {
+            weaponHandler = Instantiate(weaponPrefab, weaponPivot);
+        }
+        else
+        {
+            weaponHandler = GetComponentInChildren<WeaponHandler>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        HandleAttackDelay();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movement(movementDirection);
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.deltaTime;
+        }
+    }
+
+
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * statHandler.MoveSpeed;
+        if (knockbackDuration > 0.0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+
+        _rigidbody.velocity = direction;
+        //animationHandler.Move(direction);   Move 애니메이션
+    }
+
+    public void ApplyKnockback(Transform other, float power, float duration)  // WeaponHandler 또는 ProjectileController에서 적용
+    {
+        knockbackDuration = duration;
+        knockback = -(other.position - transform.position).normalized * power;
+    }
+
+
+    // 플레이어 또는 몬스터 공격 속도
+    private void HandleAttackDelay()
+    {
+        if (weaponHandler == null)
+        {
+            return;
+        }
+        if (timeSinceLastAttack <= statHandler.AttackFreq)
+        {
+            timeSinceLastAttack += Time.deltaTime;
+        }
+        if (isAttacking && timeSinceLastAttack > statHandler.AttackFreq)
+        {
+            timeSinceLastAttack = 0;
+            //Attack();   밑에 함수
+        }
+    }
+
+    /*
+    protected virtual void Attack()
+    {
+        if (lookDirection != Vector2.zero)
+        {
+            weaponHandler?.Attack();
+        }
+    }
+    */
+
+    public virtual void Death()
+    {
+        _rigidbody.velocity = Vector3.zero;
+
+        foreach (SpriteRenderer renderer in transform.GetComponentsInChildren<SpriteRenderer>())
+        {
+            Color color = renderer.color;
+            color.a = 0.3f;
+            renderer.color = color;
+        }
+
+        foreach (Behaviour component in transform.GetComponentsInChildren<Behaviour>())
+        {
+            component.enabled = false;
+        }
+
+        Destroy(gameObject, 2f);
     }
 }
--- Assets/Scripts/Manager/SingleTon.cs
+++ Assets/Scripts/Manager/SingleTon.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingleTon<T> where T : class, new()
+public class SingleTon<T> : MonoBehaviour where T : MonoBehaviour
 {
     private static T instance;
+
     public static T Instance
     {
         get
@@ -15,8 +16,30 @@ public class SingleTon<T> where T : class, new()
             }
             else
             {
-                return instance = new T();
+                // 첫 번째 객체 생성 시, 씬에 추가
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    instance = obj.AddComponent<T>();
+                    DontDestroyOnLoad(obj);  // 씬이 변경되어도 객체가 파괴되지 않도록 설정
+                }
+                return instance;
             }
+        }
+    }
+
+    // Awake()를 오버라이드하여 초기화 작업을 처리
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);  // 씬 전환 시 객체를 유지
+        }
+        else
+        {
+            Destroy(gameObject);  // 이미 인스턴스가 있으면 중복 객체 삭제
         }
     }
 }
