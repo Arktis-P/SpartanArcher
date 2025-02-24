c0399b10a8811588ac832c45b20a3fd57a910ecf
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-24 11:29:14 +0900

    ⚡ [ add ] 엔티티 스폰 로직 추가
    맵 매니저에서 엔티티(플레이어, 몬스터, 보스) 스폰하는 로직 추가.

--- Assets/Scripts/Manager/MapManager.cs
+++ Assets/Scripts/Manager/MapManager.cs
@@ -6,6 +6,17 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     [SerializeField] GameObject[] mapPrefabs;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject boss;
+    [SerializeField] GameObject[] monsterPrefabs;
+
+    Vector3 playerSpawnPoint = new Vector3(-7, 0, 0);
+    Vector3 bossSpawnPoint = new Vector3(25, 0, 0);
+    // values for random spawn area for monsters
+    float minMonsterSpawnX = -3f;
+    float maxMonsterSpawnX = 21f;
+    float minMonsterSpawnY = -3.5f;
+    float maxMonsterSpawnY = 3.5f;
 
     private int RandomMapCount()  // 
     {   
@@ -23,4 +34,29 @@ public class MapManager : MonoBehaviour
     }
 
     // spawn entities (player, monsters & boss)
+    public void SpawnPlayer()
+    {
+        player.transform.position = playerSpawnPoint;
+        Instantiate(player);
+    }
+    public void SpawnBoss()
+    {
+        boss.transform.position = bossSpawnPoint;
+        Instantiate(boss);
+    }
+    public void SpawnMonster()
+    {
+        // get a random monster from the list
+        int rand = Random.Range(0, monsterPrefabs.Count());
+        GameObject monster = monsterPrefabs[rand];
+
+        // get a random point in spawn area
+        float spawnPointX = Random.Range(minMonsterSpawnX, maxMonsterSpawnX);
+        float spawnPointY = Random.Range(minMonsterSpawnY, maxMonsterSpawnY);
+        Vector3 monsterSpawnPoint = new Vector3(spawnPointX, spawnPointY, 0);
+
+        monster.transform.position = monsterSpawnPoint;  // set the point
+
+        Instantiate(monster);  // spawn monster
+    }
 }
