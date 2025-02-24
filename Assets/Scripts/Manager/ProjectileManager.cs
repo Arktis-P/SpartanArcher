fc8f1c29a4378bb9534c6e30dd58f3f442853dc6
DDanPat <hyoung6855@gmail.com>
2025-02-24 20:20:08 +0900

    [ refactor ] ProjectileController.cs , ProjectileManger.cs 일부 주석 제거

--- Assets/Scripts/Manager/ProjectileManager.cs
+++ Assets/Scripts/Manager/ProjectileManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class ProjectileManager : SingleTon<ProjectileManager>
 {
-    //[SerializeField] private GameObject[] projectilePrefabs; //프로젝타일 프리팹 정렬 리스트?
+    [SerializeField]private GameObject[] projectilePrefabs; //프로젝타일 프리팹 정렬 리스트?
 
-    //public void ShootBullet(RangeWeaponHandler rangeWeaponHandler, Vector2 startPostion, Vector2 direction)
-    //{
-    //    GameObject origin = projectilePrefabs[1]; //rangeWeaponHandler.BulletIndex
-    //    GameObject obj = Instantiate(origin, startPostion, Quaternion.identity);
+    public void ShootBullet(RangeWeaponHandler rangeWeaponHandler, Vector2 startPostion, Vector2 direction)
+    {
+        GameObject origin = projectilePrefabs[1]; //rangeWeaponHandler.BulletIndex
+        GameObject obj = Instantiate(origin, startPostion, Quaternion.identity);
 
-    //    ProjectileController projectileController = obj.GetComponent<ProjectileController>();
-    //    projectileController.Init(direction, rangeWeaponHandler);
-    //}
+        ProjectileController projectileController = obj.GetComponent<ProjectileController>();
+        projectileController.Init(direction, rangeWeaponHandler);
+    }
 }
