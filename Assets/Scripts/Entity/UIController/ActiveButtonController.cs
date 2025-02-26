1735c93a23a2a1c87ba182a29288777eddeec854
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-26 17:08:13 +0900

    ⚡ [ add ] 액티브 스킬 버튼 스위치 추가

--- Assets/Scripts/Entity/UIController/ActiveButtonController.cs
+++ Assets/Scripts/Entity/UIController/ActiveButtonController.cs
@@ -1,13 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActiveButtonController : MonoBehaviour
 {
-    // only shows timer
+    // only shows timer of active skills
+    public Image activeButtonImage;
+    public ActiveSkill activeSkill;
+    public TextMeshProUGUI activeText;
 
-    public void Init()
+    public Sprite readySprite;
+    public Sprite cooldownSprite;
+
+    private void Start()
     {
+        // process errors
+        if (activeButtonImage == null || activeSkill == null || readySprite == null || cooldownSprite == null)
+        { Debug.LogError("Active Button Controller Error!"); return; }
+    }
 
+    private void Update()
+    {
+        if (activeSkill.isDashReady)  // if dash ready
+        {
+            activeButtonImage.sprite = readySprite;
+            activeButtonImage.rectTransform.sizeDelta = new Vector2(activeButtonImage.rectTransform.sizeDelta.x, 120.0f);
+            activeButtonImage.color = Color.white;
+            activeText.color = Color.white;
+        }
+        else
+        {
+            activeButtonImage.sprite = cooldownSprite;
+            activeButtonImage.rectTransform.sizeDelta = new Vector2(activeButtonImage.rectTransform.sizeDelta.x, 112.5f);
+            activeButtonImage.color = Color.grey;
+            activeText.color = Color.grey;
+        }
     }
 }
