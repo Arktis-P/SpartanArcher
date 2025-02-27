3d60c4080ef246d55f959345c2da26fa3b48b0e6
Nnwan Son <71116951+Arktis-P@users.noreply.github.com>
2025-02-27 10:51:50 +0900

    🔧 [ fix ] 스킬-버튼 연동 수정

---
+++ Assets/Scripts/Entity/UIController/ActiveButtonControllerZ.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveButtonControllerZ : ActiveButtonController
+{
+    protected override void Update()
+    {
+        if (activeSkill.isDashReady)  // if dash ready
+        {
+            activeButtonImage.sprite = readySprite;
+            activeButtonImage.rectTransform.sizeDelta = new Vector2(activeButtonImage.rectTransform.sizeDelta.x, 120.0f);
+            activeButtonImage.color = Color.white;
+            activeText.color = Color.white;
+        }
+        else
+        {
+            activeButtonImage.sprite = cooldownSprite;
+            activeButtonImage.rectTransform.sizeDelta = new Vector2(activeButtonImage.rectTransform.sizeDelta.x, 112.5f);
+            activeButtonImage.color = Color.grey;
+            activeText.color = Color.grey;
+        }
+    }
+}
